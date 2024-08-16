d5626489d3aa742e48d8af28d5501cbcd4b482ec
Asmodeous <105940384+Siddhant02082002@users.noreply.github.com>
2024-08-17 01:24:39 +0530

    Update userRoute.js

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -10,7 +10,7 @@ const router = Router();
 router.get('/', authenticate('user:read'), validate(userValidation.getUsers), catchAsync(userController.getUsers));
 router.post('/', authenticate('user:create'), validate(userValidation.createUser), catchAsync(userController.createUser));
 router.get('/:userId', authenticate('user:read'), validate(userValidation.getUser), catchAsync(userController.getUser));
-router.put('/:userId', authenticate('user:update'), validate(userValidation.updateUser), catchAsync(userController.updateUser));
+router.put('/:userValue', authenticate('user:update'), validate(userValidation.updateUser), catchAsync(userController.updateUser));
 router.delete(
 	'/:userId',
 	authenticate('user:delete'),
