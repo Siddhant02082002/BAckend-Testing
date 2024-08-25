73fba2babee9a251598a65be9061d460666a4a87
Asmodeous <105940384+Siddhant02082002@users.noreply.github.com>
2024-08-25 23:30:13 +0530

    Update zapReport.js

--- src/routes/v1/zapReport.js
+++ src/routes/v1/zapReport.js
@@ -1,1 +1,13 @@
+import { Router } from 'express';
+import catchAsync from '~/utils/catchAsync';
+import validate from '~/middlewares/validate';
+import authenticate from '~/middlewares/authenticate';
+import roleValidation from '~/validations/roleValidation';
+import roleController from '~/controllers/roleController';
 
+const router = Router();
+
+router.post('/', authenticate('role:create'), validate(roleValidation.createRole), catchAsync(roleController.createRole));
+
+
+export default router;
